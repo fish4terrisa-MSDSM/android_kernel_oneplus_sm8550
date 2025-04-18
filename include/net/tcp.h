f79e19c25223e165cb8f06f4618142ce8d4d4b04
fish4terrisa-MSDSM <flyingfish.msdsm@gmail.com>
2025-04-19 06:15:38 +0800

    Remove dup in tcp.h

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1097,9 +1097,6 @@ struct tcp_congestion_ops {
 	/* react to a specific lost skb (optional) */
 	void (*skb_marked_lost)(struct sock *sk, const struct sk_buff *skb);
 
-	/* react to a specific lost skb (optional) */
-	void (*skb_marked_lost)(struct sock *sk, const struct sk_buff *skb);
-
 	/* call when packets are delivered to update cwnd and pacing rate,
 	 * after all the ca_state processing. (optional)
 	 */
