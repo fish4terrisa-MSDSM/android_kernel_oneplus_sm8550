be42054d467e700a653136be7b9c8008d174da65
fish4terrisa-MSDSM <flyingfish.msdsm@gmail.com>
2025-12-23 00:47:35 +0800

    fix to use c89

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -692,8 +692,9 @@ static void syn_ack_recalc(struct request_sock *req,
 
 int inet_rtx_syn_ack(const struct sock *parent, struct request_sock *req)
 {
+	int err;
 	req->is_rtx = 1;
-	int err = req->rsk_ops->rtx_syn_ack(parent, req);
+	err = req->rsk_ops->rtx_syn_ack(parent, req);
 
 	if (!err)
 		req->num_retrans++;
